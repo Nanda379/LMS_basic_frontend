b8fb77422fb72e4337138f486ee958fb99f43f07
Nanda379 <boda.nandu9@gmail.com>
2024-07-09 23:48:36 +0530

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-content">
+            <p>
+                &copy; 2024 Your E-Learning Platform.All rights reserved. <br/>Made with ❤️ <a href="">Team 106</a>
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./about.css"
+
+const About = () => {
+  return (
+    <div className='about'>
+    <div className="about-content">
+        <h1>About Us</h1>
+        <h2>we are Team 106</h2>
+        <p>
+            Jyothish<br/>Dileep<br/>Yamini<br/>Nandagopal
+        </p>
+       
+    </div>
+    </div>
+  )
+}
+
+export default About
